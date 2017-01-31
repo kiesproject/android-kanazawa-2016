acacc3eed763ccbd052d3643ca8ec32c38ec2180
katsuzakimasashi <b1614211@planet.kanazawa-it.ac.jp>
2017-01-31 11:18:50 +0900

    choice_quzのレイアウト作成

--- juniorkanazawa/app/src/main/java/com/higamasa/juniorkanazawa/MainActivity.java
+++ juniorkanazawa/app/src/main/java/com/higamasa/juniorkanazawa/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.choice_quiz);
 	}
 
 	public void onButton(View view){
